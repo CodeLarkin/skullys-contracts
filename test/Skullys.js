1c83dd7f5452f49c14a619bedb2bbfcf71d3425c
Larkin <code.larkin@gmail.com>
2021-09-18 15:13:29 -0400

    more tests - public sale

--- test/Skullys.js
+++ test/Skullys.js
@@ -17,6 +17,14 @@ describe("Test harness for Skullys", function () {
         let initTime = (await provider.getBlock(blockNum)).timestamp
         await skullys.setPreSaleTime(BigNumber.from(initTime + 100000))
     }
+    async function startPublicSaleNow (provider, skullys) {
+        // set presale to 9 hours ago so public sale is live now
+        let blockNum = await provider.getBlockNumber()
+        let initTime = (await provider.getBlock(blockNum)).timestamp
+        let initDate = new Date(initTime)
+        initDate.setHours(initDate.getHours() - 9)
+        await skullys.setPreSaleTime(BigNumber.from(initDate.valueOf()))
+    }
 
     before(async function () {
         this.provider = ethers.provider;
@@ -41,6 +49,8 @@ describe("Test harness for Skullys", function () {
 
     it("Team can mint before pre-sale", async function () {
         await startPreSaleLater(this.provider, this.skullys)
+        let status = await this.skullys.getStatus()
+        expect(status).to.equal(0)  // 0 is the enum Status.Closed
 
         await this.skullys.connect(this.alice).mintFreeSkully()
         let aliceBal = await this.skullys.balanceOf(this.alice.address)
@@ -48,11 +58,29 @@ describe("Test harness for Skullys", function () {
     });
 
     it("In pre-sale, can mint a Skullys if whitelisted", async function () {
+        await startPreSaleNow(this.provider, this.skullys)
+        let status = await this.skullys.getStatus()
+        expect(status).to.equal(1)  // 1 is the enum Status.PresaleStart
         await this.skullys.connect(this.alice).setManyWhiteList([this.bobby.address, this.carly.address])
 
         await this.skullys.connect(this.bobby).mintFreeSkully()
         let bobbyBal = await this.skullys.balanceOf(this.bobby.address)
         expect(bobbyBal).to.equal(BigNumber.from(1))
+    });
+    it("In public-sale, can mint a Skullys whether whitelisted or not", async function () {
+        await startPublicSaleNow(this.provider, this.skullys)
+        await this.skullys.connect(this.alice).setManyWhiteList([this.bobby.address, this.carly.address])
+
+        await this.skullys.connect(this.bobby).mintFreeSkully()
+        await this.skullys.connect(this.bobby).mintSkullys(1, { value: ethers.utils.parseEther("25.0") })
+        await this.skullys.connect(this.dobby).mintSkullys(1, { value: ethers.utils.parseEther("25.0") })
+        await this.skullys.connect(this.erkle).mintSkullys(1, { value: ethers.utils.parseEther("25.0") })
+        let bobbyBal = await this.skullys.balanceOf(this.bobby.address)
+        let dobbyBal = await this.skullys.balanceOf(this.dobby.address)
+        let erkleBal = await this.skullys.balanceOf(this.erkle.address)
+        expect(bobbyBal).to.equal(BigNumber.from(2))
+        expect(dobbyBal).to.equal(BigNumber.from(1))
+        expect(erkleBal).to.equal(BigNumber.from(1))
     });
 
 });
